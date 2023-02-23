ba3e3dd82463c543581063518acaac44a87f5fe4
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-23 13:00:55 -0700

    Create initial variables and functions for calculations

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => {
     if (num2 === 0) {
-        return "Error. Cannot divide by zero";
+        return "Error. Cannot divide by zero!";
     }
     return num1 / num2;
 }
@@ -32,3 +32,75 @@ function operate(num1, operator, num2) {
             break;    
     }
 }
+
+// Initialize number variables
+
+const number0 = document.querySelector('.number-0');
+const number1 = document.querySelector('.number-1');
+const number2 = document.querySelector('.number-2');
+const number3 = document.querySelector('.number-3');
+const number4 = document.querySelector('.number-4');
+const number5 = document.querySelector('.number-5');
+const number6 = document.querySelector('.number-6');
+const number7 = document.querySelector('.number-7');
+const number8 = document.querySelector('.number-8');
+const number9 = document.querySelector('.number-9');
+
+// operations
+
+
+
+// display
+const display = document.querySelector('.display');
+let displayOutput = '';
+display.textContent = '0';
+
+function updateDisplay(input) {
+    displayOutput += input;
+    if (displayOutput.length > 14) {
+        return;
+    }
+    display.textContent = displayOutput;
+}
+
+
+
+number0.addEventListener('click', e => {
+    updateDisplay('0');
+});
+
+number1.addEventListener('click', e => {
+    updateDisplay('1');
+});
+
+number2.addEventListener('click', e => {
+    updateDisplay('2');
+});
+
+number3.addEventListener('click', e => {
+    updateDisplay('3');
+});
+
+number4.addEventListener('click', e => {
+    updateDisplay('4');
+});
+
+number5.addEventListener('click', e => {
+    updateDisplay('5');
+});
+
+number6.addEventListener('click', e => {
+    updateDisplay('6');
+});
+
+number7.addEventListener('click', e => {
+    updateDisplay('7');
+});
+
+number8.addEventListener('click', e => {
+    updateDisplay('8');
+});
+
+number9.addEventListener('click', e => {
+    updateDisplay('9');
+});
