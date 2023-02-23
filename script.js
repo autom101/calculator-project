1a5b937b85d6084a63970a2d498f1b84e772677a
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-23 08:39:36 -0700

    Create operate function that calls on basic math functions

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ const divide = (num1, num2) => {
     }
     return num1 / num2;
 }
+
+// Operate function
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            break;    
+    }
+}
