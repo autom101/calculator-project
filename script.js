7efb01efda37038ea9e8fd16672d370bf274b939
sojha2 <122489814+sojha2@users.noreply.github.com>
2023-02-23 08:30:46 -0700

    Created basic math functions

---
+++ script.js
@@ -0,0 +1,14 @@
+// Basic math operations
+
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+const multiply = (num1, num2) => num1 * num2;
+
+const divide = (num1, num2) => {
+    if (num2 === 0) {
+        return "Error. Cannot divide by zero";
+    }
+    return num1 / num2;
+}
